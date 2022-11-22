af7562d86892c2d5c6f383b7d5badfcd6afc1e95
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-22 13:50:58 +0200

    done first quest!

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const refs = {
+    btnSrart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+};
+function desactiveButtonOnStart() {
+    refs.btnStop.setAttribute('disabled', null);   
+};
+desactiveButtonOnStart();
+
+refs.btnSrart.addEventListener('click', onButtonClick);
+refs.btnStop.addEventListener('click', onButtonClick);
+
+function onButtonClick(e) {
+    if (e.target.dataset.start === '') {
+        console.log('start');
+        changeActiveButton();
+        startColorChanges();
+        intervalId = setInterval(startColorChanges, 1000);
+    };
+    if (e.target.dataset.stop === '') {
+        console.log('stop');
+        changeActiveButton(); 
+        clearInterval(intervalId);
+    };
+};
+
+function changeActiveButton() {
+    refs.btnSrart.toggleAttribute('disabled');
+    refs.btnStop.toggleAttribute('disabled');
+};
+
+function startColorChanges() {
+    document.body.style.backgroundColor = getRandomHexColor();
+};
+
 
