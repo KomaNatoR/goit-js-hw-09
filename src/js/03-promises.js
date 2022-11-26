c4a70084f55a04eb7fc194bce430750e2a137b1e
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-26 09:16:10 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,35 +1,35 @@
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
 
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
-//       } else {
-//         reject(Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
-//       }
-//     }, delay);
-//   });
-// };
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
+      } else {
+        reject(Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+      }
+    }, delay);
+  });
+};
 
-// const refs = {
-//   form: document.querySelector('.form'),
-// };
+const refs = {
+  form: document.querySelector('.form'),
+};
 
-// refs.form.addEventListener('submit', onButtonSubmit);
+refs.form.addEventListener('submit', onButtonSubmit);
 
-// function onButtonSubmit(e) {
-//   e.preventDefault();
-//   const delay = e.currentTarget.elements.delay.value;
-//   const step = e.currentTarget.elements.step.value;
-//   const amount = e.currentTarget.elements.amount.value;
+function onButtonSubmit(e) {
+  e.preventDefault();
+  const delay = e.currentTarget.elements.delay.value;
+  const step = e.currentTarget.elements.step.value;
+  const amount = e.currentTarget.elements.amount.value;
 
-//   for (let i = 0; i < amount; i += 1) {
-//     let position = i + 1;
-//     let changingDelay =Number(delay)+Number(step)*i;
+  for (let i = 0; i < amount; i += 1) {
+    let position = i + 1;
+    let changingDelay =Number(delay)+Number(step)*i;
 
-//     createPromise(position,changingDelay).then(succes=>succes).catch(error=>error);
-//   }
-// };
+    createPromise(position,changingDelay).then(succes=>succes).catch(error=>error);
+  }
+};
