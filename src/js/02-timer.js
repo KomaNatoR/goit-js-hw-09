7609b488de3f10af985f1a32954a36811d1a2082
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-26 09:15:07 +0200

    фф

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,97 +1,97 @@
-// Описаний в документації
-import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
+// // Описаний в документації
+// import flatpickr from "flatpickr";
+// // Додатковий імпорт стилів
+// import "flatpickr/dist/flatpickr.min.css";
 
-//  style="display: flex;flex-direction: column"
-const bodyTimer = ({ days, hours, minutes, seconds }) => `
-  <div class="field">
-    <span class="value" data-days>${String(days).length < 2? '0'+days:days}</span>
-    <span class="label">Days</span>
-  </div>
-  <div class="field">
-    <span class="value" data-hours>${String(hours).length < 2? '0'+hours:hours}</span>
-    <span class="label">Hours</span>
-  </div>
-  <div class="field">
-    <span class="value" data-minutes>${String(minutes).length < 2? '0'+minutes:minutes}</span>
-    <span class="label">Minutes</span>
-  </div>
-  <div class="field">
-    <span class="value" data-seconds>${String(seconds).length < 2? '0'+seconds:seconds}</span>
-    <span class="label">Seconds</span>
-  </div>`;
+// //  style="display: flex;flex-direction: column"
+// const bodyTimer = ({ days, hours, minutes, seconds }) => `
+//   <div class="field">
+//     <span class="value" data-days>${String(days).length < 2? '0'+days:days}</span>
+//     <span class="label">Days</span>
+//   </div>
+//   <div class="field">
+//     <span class="value" data-hours>${String(hours).length < 2? '0'+hours:hours}</span>
+//     <span class="label">Hours</span>
+//   </div>
+//   <div class="field">
+//     <span class="value" data-minutes>${String(minutes).length < 2? '0'+minutes:minutes}</span>
+//     <span class="label">Minutes</span>
+//   </div>
+//   <div class="field">
+//     <span class="value" data-seconds>${String(seconds).length < 2? '0'+seconds:seconds}</span>
+//     <span class="label">Seconds</span>
+//   </div>`;
 
-const refs = {
-    startBtn: document.querySelector('[data-start]'),
-    mainDiv: document.querySelector('.timer'),
-};
+// const refs = {
+//     startBtn: document.querySelector('[data-start]'),
+//     mainDiv: document.querySelector('.timer'),
+// };
 
-let userData = 0;
-let newData = new Date();
+// let userData = 0;
+// let newData = new Date();
 
-// =================================== НЕ МОЄ =============================
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-    onClose(selectedDates) {
-      userData = selectedDates[0].getTime();
-      refs.startBtn.disabled = false;
+// // =================================== НЕ МОЄ =============================
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//     onClose(selectedDates) {
+//       userData = selectedDates[0].getTime();
+//       refs.startBtn.disabled = false;
 
-      if (userData < newData) {
-        refs.startBtn.disabled = true;
-        alert('Та ти успакойся!!!');
-      }
-      if (userData > newData) {
-        alert('Я пишаюсь тобою!!!');
-      }
-    },
-};
-const flatpickr = flatpickr("#datetime-picker", options);
+//       if (userData < newData) {
+//         refs.startBtn.disabled = true;
+//         alert('Та ти успакойся!!!');
+//       }
+//       if (userData > newData) {
+//         alert('Я пишаюсь тобою!!!');
+//       }
+//     },
+// };
+// const flatpickr = flatpickr("#datetime-picker", options);
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  return { days, hours, minutes, seconds };
-};
+//   return { days, hours, minutes, seconds };
+// };
 
-// =================================== МОЄ ======================================
-refs.startBtn.addEventListener('click', onStartClick);
+// // =================================== МОЄ ======================================
+// refs.startBtn.addEventListener('click', onStartClick);
 
-function onStartClick() {
-    toggleStartButton();
-    intervaId = setInterval(() => {
-        newData = new Date();
-        const numberToCountTime = userData - newData;
-        refs.mainDiv.innerHTML = bodyTimer(convertMs(numberToCountTime));
-        if (numberToCountTime < 1000) {
-            clearInterval(intervaId);
-            refs.startBtn.disabled= false;
-        }
-    }, 1000);
+// function onStartClick() {
+//     toggleStartButton();
+//     intervaId = setInterval(() => {
+//         newData = new Date();
+//         const numberToCountTime = userData - newData;
+//         refs.mainDiv.innerHTML = bodyTimer(convertMs(numberToCountTime));
+//         if (numberToCountTime < 1000) {
+//             clearInterval(intervaId);
+//             refs.startBtn.disabled= false;
+//         }
+//     }, 1000);
 
-    // console.log(convertMs(numberForCountTime));
-    // console.log(numberForCountTime);
-};
+//     // console.log(convertMs(numberForCountTime));
+//     // console.log(numberForCountTime);
+// };
 
-function toggleStartButton() {
-    refs.startBtn.toggleAttribute('disabled');
-};
+// function toggleStartButton() {
+//     refs.startBtn.toggleAttribute('disabled');
+// };
 
 
-// console.log(userData);
+// // console.log(userData);
