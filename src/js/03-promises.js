212b874354579876f6d1623736b3bb5e39e53300
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-11-26 04:39:25 +0200

    3 almost done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,31 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  setTimeout(() => {
+    if (shouldResolve) {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  }, delay);
 };
 
 const refs = {
   form: document.querySelector('.form'),
 };
 
-refs.form.addEventListener('submit', createPromise);
+refs.form.addEventListener('submit', onButtonSubmit);
+
+function onButtonSubmit(e) {
+  e.preventDefault();
+  const delay = e.currentTarget.elements.delay.value;
+  const step = e.currentTarget.elements.step.value;
+  const amount = e.currentTarget.elements.amount.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    let position = i + 1;
+    let changingDelay =Number(delay)+Number(step)*i;
+
+    createPromise(position,changingDelay);
+  }
+  
+}
